8c551acb664609f6e9c6641f74d7a85ffba2a1ef
Samantha August <samiaugust@gmail.com>
2015-09-09 19:10:48 -0400

    initial commit with resources

---
+++ app/views/holes/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @hole, :id, :name, :par, :course_id, :par, :scorecard_id, :created_at, :updated_at
---
+++ db/migrate/20150909225844_create_holes.rb
@@ -0,0 +1,13 @@
+class CreateHoles < ActiveRecord::Migration
+  def change
+    create_table :holes do |t|
+      t.string :name
+      t.integer :par
+      t.integer :course_id
+      t.integer :par
+      t.integer :scorecard_id
+
+      t.timestamps null: false
+    end
+  end
+end
